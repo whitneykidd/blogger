584e76ef808f1fd7d84064d8982ce7875448daa4
Whitney Kidd <whitneymkidd@gmail.com>
2020-05-03 15:22:40 -0600

    Create user_sees_one_article spec

---
+++ spec/features/user_sees_one_article_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+describe "user sees one article" do
+  describe "they link from the articles index" do
+    it "displays information for one article" do
+      article_1 = Article.create!(title: "Title 1", body: "Body 1")
+
+      visit articles_path
+
+      click_link article.title
+
+      expect(page).to have_content(article.title)
+      expect(page).to have_content(article.body)
+
+    end
+  end
+end
